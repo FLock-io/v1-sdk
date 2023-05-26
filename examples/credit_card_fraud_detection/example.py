4da548a00fd7299d05eff60f01a39fb1cf47313e
Ray Sun <ray.ruisun@qq.com>
2023-05-26 12:32:16 +0100

    Update dgc to latest version

--- examples/credit_card_fraud_detection/example.py
+++ examples/credit_card_fraud_detection/example.py
@@ -62,28 +62,23 @@ class FlockModel:
     4. Output the model parameters retrained on the dataset AS BYTES
     """
 
-    def train(self, parameters: bytes | None, dataset: list[dict]) -> bytes:
+    def train(self, compressed_gradients: bytes | None, dataset: list[dict]) -> bytes:
         data_loader = self.process_dataset(self.train_set)
         # data_loader = self.fabric.setup_dataloader(data_loader)
 
-        if parameters is not None:
-            self.model.load_state_dict(torch.load(io.BytesIO(parameters)))
+        if compressed_gradients is not None:
+            compressed_grads = torch.load(io.BytesIO(compressed_gradients))
+            for p, compressed_grad in zip(self.model.parameters(), compressed_grads):
+                p.data.add_(compressed_grad, alpha=self.lr)
 
         self.model.train()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         criterion = torch.nn.BCELoss()
         self.model.to(self.device)
 
-        # size_before_compression = 0
-        # for p in self.model.parameters():
-        #     if p.grad is not None:
-        #         size_before_compression += p.grad.element_size() * p.grad.nelement()
-        #     else:
-        #         logger.info("None grad")
-
-        buffer = io.BytesIO()
-        torch.save(self.model.state_dict(), buffer)
-        uncompressed_payload = buffer.getvalue()
+        uncompressed_buffer = io.BytesIO()
+        torch.save(self.model.state_dict(), uncompressed_buffer)
+        uncompressed_payload = uncompressed_buffer.getvalue()
 
 
         for epoch in range(self.epochs):
@@ -101,45 +96,31 @@ class FlockModel:
                 loss.backward()
                 # self.fabric.backward(loss)
 
-                # Compress gradients
-                grads = [p.grad for p in self.model.parameters()]
-                compressed_grads = dgc(grads)
-                # Manually update model parameters
-                for p, compressed_grad in zip(self.model.parameters(), compressed_grads):
-                    p.data.add_(-self.lr, compressed_grad)
-
                 optimizer.step()
 
                 train_loss += loss.item() * inputs.size(0)
                 predicted = torch.round(outputs).squeeze()
                 train_total += targets.size(0)
                 train_correct += (predicted == targets.squeeze()).sum().item()
+                if batch_idx < 2:
+                    logger.debug(f"Batch {batch_idx}, Acc: {round(100.0 * train_correct / train_total, 2)}, Loss: {round(train_loss / train_total, 4)}")
 
             logger.info(
                 f"Training Epoch: {epoch}, Acc: {round(100.0 * train_correct / train_total, 2)}, Loss: {round(train_loss / train_total, 4)}"
             )
 
-        # size_after_compression = 0
-        # for p in self.model.parameters():
-        #     if p.grad is not None:
-        #         size_after_compression += p.grad.element_size() * p.grad.nelement()
-        #     else:
-        #         logger.info("None grad")
-        # delta_compression_size = size_before_compression - size_after_compression
-        # logger.info(f"Delta Compression size: {self.payload_size_reformat(delta_compression_size)}, "
-        #             f"compressed ratio: {round(delta_compression_size / size_before_compression * 100,2) if round(delta_compression_size / size_before_compression * 100,2) !=0 else 0}%, "
-        #             f"original size: {self.payload_size_reformat(size_before_compression)}, "
-        #             f"compressed size: {self.payload_size_reformat(size_after_compression)}")
+        grads = [p.grad for p in self.model.parameters()]
+        compressed_grads = dgc(grads)
 
-        buffer = io.BytesIO()
-        torch.save(self.model.state_dict(), buffer)
+        compressed_buffer = io.BytesIO()
+        torch.save(compressed_grads, compressed_buffer)
         # return buffer.getvalue()
-        compressed_payload = buffer.getvalue()
-        logger.info(f"Delta Compression size: {self.payload_size_reformat(sys.getsizeof(uncompressed_payload) -  sys.getsizeof(compressed_payload))}, "
-                    f"compressed ratio: {round((sys.getsizeof(uncompressed_payload) -  sys.getsizeof(compressed_payload)) / sys.getsizeof(uncompressed_payload) * 100,2)}%, "
-                    f"original size: {self.payload_size_reformat(uncompressed_payload)}, "
-                    f"compressed size: {self.payload_size_reformat(compressed_payload)}")
-
+        compressed_payload = compressed_buffer.getvalue()
+        logger.info(
+            f"Delta Compression size: {self.payload_size_reformat(len(uncompressed_payload) - len(compressed_payload))}, "
+            f"compressed ratio: {round((len(uncompressed_payload) - len(compressed_payload)) / len(uncompressed_payload) * 100, 2)}%, "
+            f"original size: {self.payload_size_reformat(len(uncompressed_payload))}, "
+            f"compressed size: {self.payload_size_reformat(len(compressed_payload))}")
         return compressed_payload
 
     """
@@ -150,12 +131,15 @@ class FlockModel:
     5. Output the accuracy of the model parameters on the dataset as a float
     """
 
-    def evaluate(self, parameters: bytes | None, dataset: list[dict]) -> float:
+    def evaluate(self, compressed_gradients: bytes | None, dataset: list[dict]) -> float:
         data_loader = self.process_dataset(self.test_set)
         criterion = torch.nn.BCELoss()
 
-        if parameters is not None:
-            self.model.load_state_dict(torch.load(io.BytesIO(parameters)))
+        if compressed_gradients is not None:
+            compressed_grads = torch.load(io.BytesIO(compressed_gradients))
+            for p, compressed_grad in zip(self.model.parameters(), compressed_grads):
+                p.data.add_(compressed_grad, alpha=self.lr)
+
         self.model.to(self.device)
         self.model.eval()
 
@@ -181,39 +165,43 @@ class FlockModel:
     aggregate them using avg and output the aggregated parameters as bytes.
     """
 
-    def aggregate(self, parameters_list: list[bytes]) -> bytes:
-        parameters_list = [
-            torch.load(io.BytesIO(parameters)) for parameters in parameters_list
-        ]
-        averaged_params_template = parameters_list[0]
-        for k in averaged_params_template.keys():
-            temp_w = []
-            for local_w in parameters_list:
-                temp_w.append(local_w[k])
-            averaged_params_template[k] = sum(temp_w) / len(temp_w)
+    def aggregate(self, compressed_gradients_list: list[bytes]) -> bytes:
+
+        gradients_list = [torch.load(io.BytesIO(compressed_grads_bytes)) for compressed_grads_bytes in
+                          compressed_gradients_list]
+
+        # logger.info(f"len gradients_list {len(gradients_list)}")
+        # logger.info(f"gradients_list : {gradients_list}")
+        #
+        # logger.info(f"gradients_list[0][0].shape : {gradients_list[0][0].shape}")
+        # logger.info(f"gradients_list[1][0].shape : {gradients_list[1][0].shape}")
+
+        transposed_gradients_list = list(map(list, zip(*gradients_list)))
+
+        averaged_gradients = [torch.stack(tensors).mean(dim=0) for tensors in transposed_gradients_list]
 
         # Create a buffer
         buffer = io.BytesIO()
 
         # Save state dict to the buffer
-        torch.save(averaged_params_template, buffer)
+        torch.save(averaged_gradients, buffer)
 
         # Get the byte representation
         aggregated_parameters = buffer.getvalue()
 
         return aggregated_parameters
 
+
     def payload_size_reformat(self, payload):
         # Monitor size of model
-        if int(sys.getsizeof(payload) / 1024) == 0:
-            return f'{round(sys.getsizeof(payload),3)} b'
-        elif int(sys.getsizeof(payload) / 1024 / 1024) == 0:
-            return f'{round(sys.getsizeof(payload) / 1024,3)} Kb'
-        elif int(sys.getsizeof(payload) / 1024 / 1024 / 1024) == 0:
-            return f'{round(sys.getsizeof(payload) / 1024 / 1024,3)} Mb'
+        if int(payload / 1024) == 0:
+            return f'{round(payload,3)} b'
+        elif int(payload / 1024 / 1024) == 0:
+            return f'{round(payload / 1024,3)} Kb'
+        elif int(payload / 1024 / 1024 / 1024) == 0:
+            return f'{round(payload / 1024 / 1024,3)} Mb'
         else:
-            return f'{round(sys.getsizeof(payload) / 1024 / 1024 / 1024,3)} Gb'
-
+            return f'{round(payload / 1024 / 1024 / 1024,3)} Gb'
 
 if __name__ == "__main__":
     """
