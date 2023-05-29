018f1794ac4cc5cb15fdb0d58e3434534a327236
Ray Sun <ray.ruisun@qq.com>
2023-05-30 00:46:49 +0200

    Update basic_cnn.py

--- examples/credit_card_fraud_detection/models/basic_cnn.py
+++ examples/credit_card_fraud_detection/models/basic_cnn.py
@@ -24,8 +24,35 @@ class CreditFraudNetMLP(nn.Module):
         )
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc2(x)
         output = self.fc3(x)
         return output
+
+    
+# class CreditFraudNetMLP(nn.Module):
+#     def __init__(self, num_features, num_classes):
+#         super(CreditFraudNetMLP, self).__init__()
+#         self.fc1 = nn.Sequential(
+#             nn.Linear(num_features, 64),
+#             nn.ReLU(),
+#             nn.Dropout(0.2)
+#         )
+
+#         self.fc2 = nn.Sequential(
+#             nn.Linear(64, 128),
+#             nn.ReLU(),
+#             nn.Dropout(0.5)
+#         )
+
+#         self.fc3 = nn.Sequential(
+#             nn.Linear(128, num_classes),
+#             nn.Sigmoid()
+#         )
+
+#     def forward(self, x):
+#         x = x.view(x.size(0), -1)
+#         x = self.fc1(x)
+#         x = self.fc2(x)
+#         output = self.fc3(x)
+#         return output
