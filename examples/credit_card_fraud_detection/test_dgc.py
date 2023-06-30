6b8063c82117438e95c4b097437f7af34ede54de
limberc <zehua.cheng@cs.ox.ac.uk>
2023-06-30 23:11:45 +0100

    Add `test_dgc_sgd_save_all_grad`

--- examples/credit_card_fraud_detection/test_dgc.py
+++ examples/credit_card_fraud_detection/test_dgc.py
@@ -3,7 +3,7 @@ import unittest
 import torch
 import torch.nn.functional as F
 
-from .compresser import DGC_SGD
+from .compresser import DGC_SGD, dgc_func
 from .models.basic_cnn import CreditFraudNetMLP
 
 
@@ -13,9 +13,9 @@ class TestDGCSGD(unittest.TestCase):
         self.optimizer = DGC_SGD(self.model.parameters(), lr=0.01, momentum=0.9)
 
     def test_dgc_sgd(self):
-        input = torch.randn(2000, 29)
+        inputs = torch.randn(2000, 29)
         target = torch.randn(2000, 1)
-        output = self.model(input)
+        output = self.model(inputs)
         # Compute loss
         loss = F.binary_cross_entropy(output, target)
         # Backward pass
@@ -28,6 +28,22 @@ class TestDGCSGD(unittest.TestCase):
         # Get the gradient
         compressed_grads = self.optimizer.compressed_grads
         print(f"After Optimizer.step - {compressed_grads[0]}")
+
+    def test_dgc_sgd_save_all_grad(self):
+        # Save the 200 batch gradients, each batch has 200 samples. NO DGC gradient update.
+        features = torch.randn(200, 200, 29)
+        targets = torch.randn(200, 200, 1)
+        datasets = zip(features, targets)
+        # save all gradients
+        all_grads = []
+        for feature, target in datasets:
+            outs = self.model(feature)
+            loss_val = F.binary_cross_entropy(outs, target)
+            loss_val.backward()
+            uncompressed_grad = [param.grad.clone() for param in self.model.parameters()]
+            compressed_grad = dgc_func(uncompressed_grad)
+            all_grads.append(uncompressed_grad)
+        torch.save(all_grads, "all_uncompressed_grads.pt")
 
 
 if __name__ == '__main__':
