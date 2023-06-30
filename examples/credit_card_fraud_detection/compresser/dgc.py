018d876344dc66a33c473e90e52e6358bf9b4009
limberc <zehua.cheng@cs.ox.ac.uk>
2023-06-30 22:54:26 +0100

    Add dgc_function.

--- examples/credit_card_fraud_detection/compresser/dgc.py
+++ examples/credit_card_fraud_detection/compresser/dgc.py
@@ -2,6 +2,37 @@ import torch
 from torch.optim import SGD
 
 
+def dgc_func(grads, sparsity: float = 0.9):
+    """
+    This function implements the Deep Gradient Compression (DGC) for a given set of gradients.
+    Args:
+    grads: list of gradients to compress
+    sparsity: the desired sparsity level, a float between 0 and 1.
+    """
+    # Flatten the gradients into a single 1-D tensor
+    flat_grads = torch.cat([grad.view(-1) for grad in grads])
+
+    # Compute the threshold
+    abs_grads = flat_grads.abs()
+    k = int(sparsity * flat_grads.numel())
+    threshold = abs_grads.topk(k, largest=False).values.max()
+
+    # Create a mask for the elements to keep
+    mask = abs_grads.gt(threshold).float()
+
+    # Apply the mask to the original gradients
+    compressed_grads = []
+    start = 0
+    for grad in grads:
+        end = start + grad.numel()
+        compressed_grad = grad * mask[start:end].view_as(grad)
+        compressed_grads.append(compressed_grad)
+        start = end
+    sparse_tensors = [compressed_grad.to_sparse() for compressed_grad in compressed_grads]
+
+    return sparse_tensors
+
+
 class DGC_SGD(SGD):
     def __init__(self, params, lr, momentum=0, dampening=0,
                  weight_decay=0, nesterov=False, sparsity=0.01, gradient_clip=0.01,
@@ -38,10 +69,6 @@ class DGC_SGD(SGD):
                 if p in self.state and 'momentum_buffer' in self.state[p]:
                     buf = self.state[p]['momentum_buffer']
                     buf.mul_(mask)
-
-                # Gradient Alignment
-                if p.grad.data.norm() != 0:
-                    p.grad.data /= p.grad.data.norm()
 
                 if weight_decay != 0:
                     d_p.add_(p, alpha=weight_decay)
