6271b149eb3f6d1739ef6a3de7e675ceb274901d
Ray <ray.ruisun@qq.com>
2023-07-27 12:12:29 +0100

    fit problem

--- examples/flock_llm/build_and_upload.sh
+++ examples/flock_llm/build_and_upload.sh
@@ -10,7 +10,7 @@ echo "Saving the docker image to a file and compressing it. It may take a while.
 time (docker save $IMAGE_TAG | xz -T 0 > $OUTPUT_FILE)
 
 echo "Uploading the compressed image to IPFS.."
- json=`curl -F "file=@$OUTPUT_FILE" ipfs.flock.io/api/v0/add`
+json=`curl -F "file=@$OUTPUT_FILE" ipfs.flock.io/api/v0/add`
 
 # Uncomment if you'd like to upload to your local IPFS
 #json=`curl -F "file=@$OUTPUT_FILE" 127.0.0.1:5001/api/v0/add`
